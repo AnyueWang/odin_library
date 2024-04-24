74d00434168d7b209d64f6b7b38da8454ce7b3cb
AnyueWang <moonwong.way@gmail.com>
2024-04-24 11:03:45 +0200

    v0.1 Basic functions updates Styles improved. Some functions are added compared to v0.0:     1. Add a delete button to each book card. The "delete" button also makes changes to myLibrary array;     2. Add read/unread toggle button to each book card. Once read/unread is toggles, changes will be made in the corresponding Book object;     3. Prevent "submit" action when adding a new book (waiting for future updates).

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const btnNew = document.querySelector(".btn-new");
 const btnConfirm = document.querySelector(".btn-confirm");
 const btnCancel = document.querySelector(".btn-cancel");
 const containerBooks = document.querySelector(".container-books");
+const newInput = document.querySelector("#new-input");
+const infoTags = {
+    author: "Author:",
+    title: "Title:",
+    pages: "Pages:",
+    read: "Have you read it?"
+};
 
 function Book(author, title, pages, read) {
     this.author = author;
@@ -13,14 +20,18 @@ function Book(author, title, pages, read) {
     this.read = read;
 }
 
+Book.prototype.toggleRead = function () {
+    this.read = !this.read;
+}
+
 myLibrary.push(new Book("Patrick Rothfuss", "The Name of the Wind", 793, false));
 myLibrary.push(new Book("J.K. Rowling", "Harry Potter and the Deathly Hallows", 1302, true));
 myLibrary.push(new Book("Brandon Sanderson", "The Final Empire", 980, false));
 myLibrary.push(new Book("George R.R. Martin", "A Storm of Swords", 2031, true));
 myLibrary.push(new Book("Scott Lynch", "The Lies of Locke Lamora", 698, false));
 
-function addBookToLibrary() {
-
+function addBookToLibrary(newBook) {
+    myLibrary.push(newBook);
 }
 
 btnNew.addEventListener("click", () => {
@@ -31,16 +42,58 @@ btnCancel.addEventListener("click", () => {
     dialog.close();
 });
 
-myLibrary.forEach(eachBook => {
+myLibrary.forEach(showBookCard);
+
+btnConfirm.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(newInput);
+    const data = Object.fromEntries(formData);
+    const newBook = new Book(data.newAuthor, data.newTitle, data.newPages, Boolean(data.newRead));
+    addBookToLibrary(newBook);
+    showBookCard(newBook);
+
+    dialog.close();
+});
+
+function showBookCard(book) {
     const newCard = document.createElement("card");
     const newList = document.createElement("ul");
-    for (let key in eachBook) {
-        const newContent = document.createElement("li");
-        const keyFormatted = key === "read" ? "Have you read it?" : key[0].toUpperCase() + key.slice(1) + ":";
-        const value = key === "read" ? (eachBook[key] === true ? "Yes" : "No") : eachBook[key];
-        newContent.textContent = `${keyFormatted} ${value}`;
-        newList.appendChild(newContent);
+    for (let key in book) {
+        if (book.hasOwnProperty(key)) {
+            const newContent = document.createElement("li");
+            const keyFormatted = infoTags[key] ;
+            const value = key === "read" ? (book[key] === true ? "Yes" : "No") : book[key];
+            newContent.textContent = `${keyFormatted} ${value}`;
+            newList.appendChild(newContent);
+        }
     }
     newCard.appendChild(newList);
+
+    const btnDelete = document.createElement("button");
+    btnDelete.textContent = "Delete";
+    newCard.appendChild(btnDelete);
+
+    const btnToggleRead = document.createElement("button");
+    btnToggleRead.textContent = showIsRead(book);
+    newCard.appendChild(btnToggleRead);
+
     containerBooks.appendChild(newCard);
-});
+
+    btnDelete.addEventListener("click", () => {
+        containerBooks.removeChild(newCard);
+        const indexDelete = myLibrary.indexOf(book);
+        myLibrary.splice(indexDelete, 1);
+    });
+
+    btnToggleRead.addEventListener("click", () => {
+        book.toggleRead();
+        newCard.firstElementChild.lastElementChild.textContent = `${infoTags.read} ${book.read === true ? "Yes" : "No"}`;
+        btnToggleRead.textContent = showIsRead(book);
+    });
+}
+
+function showIsRead(book) {
+    return book.read ? "Not Read" : "Read";
+}
+
